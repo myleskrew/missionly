b4e627ce42dd5d0420110944be8b83efe6507caf
mylestnc-create <mylestnc@gmail.com>
2026-06-19 15:38:28 -0600

    Remove debug test endpoints

--- api/ping.ts
+++
@@ -1,3 +0,0 @@
-export default function handler(req: any, res: any) {
-  res.json({ pong: true, node: process.version, env: Object.keys(process.env).filter(k => k.startsWith('STRIPE')).join(',') });
-}
